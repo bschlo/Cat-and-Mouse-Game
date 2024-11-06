60517008e03a07fea940a035a841ac33244ccbf5
bschloss <benschloss2@gmail.com>
2024-11-06 10:55:29 -0500

    Adding updated win and next turn message.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Cat-and-Mouse-Game
+
+## Project Description
+
+"Cat and Mouse Game" is an online one-player versus computer game created to test the users ability to avoid the computers next move. The mouse (user) must collect all nine cheeses without landing on the same square as the cat (computer) to win. If the mouse is caught by the cat, then the mouse loses.
+
+For the optimal experience, the game is best played in a Google Chrome web browser.
+
+The game was built using HTML, CSS, and Javascript.
+
+## Play Now
+
+Deployed Website: https://glittering-starburst-88834e.netlify.app/
+
+GitHub Repo: https://github.com/bschlo/Cat-and-Mouse-Game
+
+### How to Play 
+
--- js/app.js
+++ js/app.js
@@ -1,155 +1,160 @@
-let board = []
-let mouseTracker = []
-let winner = false
-let loser = false
-let mouse = '🐁'
-let cat = '🐈'
-let cheese = '🧀'
-let mousePosition = 8
-let catPosition = 0
-let mouseScore = 0
-let catScore = 0
-let gameOver = false
+let board = [];
+let mouseTracker = [];
+let winner = false;
+let loser = false;
+let mouse = "🐁";
+let cat = "🐈";
+let cheese = "🧀";
+let mousePosition = "";
+let catPosition = "";
+let mouseScore = 0;
+let catScore = 0;
+let gameOver = false;
 
-
-
-const squareEls = document.querySelectorAll(".sqr")
-const messageEl = document.querySelector("#message")
-const resetBtn = document.querySelector("#reset")
-const mouseTally = document.querySelector("#mouse-tally")
-const catTally = document.querySelector("#cat-tally")
-
-
+const squareEls = document.querySelectorAll(".sqr");
+const messageEl = document.querySelector("#message");
+const resetBtn = document.querySelector("#reset");
+const mouseTally = document.querySelector("#mouse-tally");
+const catTally = document.querySelector("#cat-tally");
 
 const init = () => {
-    board = [cheese,cheese,cheese,cheese,cheese,cheese,cheese,cheese,cheese]
-    winner = false
-    loser = false
-    mouseTracker = []
-    render ()
-}
-
+  board = [
+    cheese,
+    cheese,
+    cheese,
+    cheese,
+    cheese,
+    cheese,
+    cheese,
+    cheese,
+    cheese,
+  ];
+  winner = false;
+  loser = false;
+  mouseTracker = [];
+  render();
+};
 const render = () => {
-    updateBoard()
-    updateMessage()
-}
+  updateBoard();
+  updateMessage();
+};
 
 const updateBoard = () => {
-    board.forEach((box, index) => {
-      const square = squareEls[index];
-  
-      if (box === cat) {
-        square.innerText = cat;
-      } else if (box === mouse) {
-        square.innerText = mouse;
-      } else if (box === cheese) {
-        square.innerText = cheese;
-      } else {
-        square.innerText = ''
-      }
-    });
-  };
+  board.forEach((box, index) => {
+    const square = squareEls[index];
+
+    if (box === cat) {
+      square.innerText = cat;
+    } else if (box === mouse) {
+      square.innerText = mouse;
+    } else if (box === cheese) {
+      square.innerText = cheese;
+    } else {
+      square.innerText = "";
+    }
+  });
+};
 
 const updateMessage = () => {
-    if (winner === false && loser === true && gameOver === true) {
-        messageEl.innerText = `THE CAT GOT YOU. Click the reset button to start a new game.`
-    } else if (winner === true && gameOver === true) {
-        messageEl.innerText = `CONGRATS YOU GOT ALL THE CHEESE`
-        winner = false
-    } else if (winner === false && loser === false) {
-       messageEl.innerText = `Collect All The Cheese To Win`
-    }
-}
+  if (winner === false && loser === true && gameOver === true) {
+    messageEl.innerText = `The cat got you. Click the reset button to start a new game.`;
+  } else if (winner === true && gameOver === true) {
+    messageEl.innerText = `Congrats you got all the cheese. Click the reset button to start a new game.`;
+    winner = false;
+  } else if (winner === false && loser === false) {
+    messageEl.innerText = `Collect all the cheese to win by clicking the squares and avoid the cat.`;
+  }
+};
 
-
-
-const winningCombo = [0,1,2,3,4,5,6,7,8]
+const winningCombo = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const handleClick = (event) => {
-    if (gameOver === true) {
-        return
-    }
-    const squareIndex = parseInt(event.target.id)
-    mousePlacePiece(squareIndex)
-    updateMouseTracker(squareIndex)
-    catPlacePiece()
-    checkIfWinner()
-    render()
-}
+  if (gameOver === true) {
+    return;
+  }
+  const squareIndex = parseInt(event.target.id);
+  mousePlacePiece(squareIndex);
+  updateMouseTracker(squareIndex);
+  catPlacePiece();
+  checkIfWinner();
+  render();
+};
 
 const movesAllowed = {
-    0: [1, 3],
-    1: [0, 2, 4],
-    2: [1, 5],
-    3: [0, 4, 6],
-    4: [1, 3, 5, 7],
-    5: [2, 4, 8],
-    6: [3, 7],
-    7: [4, 6, 8],
-    8: [5, 7]
-}
+  0: [1, 3],
+  1: [0, 2, 4],
+  2: [1, 5],
+  3: [0, 4, 6],
+  4: [1, 3, 5, 7],
+  5: [2, 4, 8],
+  6: [3, 7],
+  7: [4, 6, 8],
+  8: [5, 7],
+};
 
 const catPlacePiece = () => {
-    const previousCatPosition = board.indexOf(cat) 
+  const previousCatPosition = board.indexOf(cat);
 
-        if (previousCatPosition !== -1) {
-                board[previousCatPosition] = previousSquareValue
-        }
-        newCatPosition = Math.floor(Math.random() * 9)
-        previousSquareValue = board[newCatPosition]
-        catPosition = newCatPosition
-        board[catPosition] = cat
-}
+  if (previousCatPosition !== -1) {
+    board[previousCatPosition] = previousSquareValue;
+  }
+  newCatPosition = Math.floor(Math.random() * 9);
+  previousSquareValue = board[newCatPosition];
+  catPosition = newCatPosition;
+  board[catPosition] = cat;
+};
 
 const mousePlacePiece = (index) => {
-    const previousMousePosition = board.indexOf(mouse);
+  const previousMousePosition = board.indexOf(mouse);
 
-    if (previousMousePosition !== -1) {
-        board[previousMousePosition] = '';
-    }
-    board[index] = mouse;
-    mousePosition = index;
-}
+  if (previousMousePosition !== -1) {
+    board[previousMousePosition] = "";
+  }
+  board[index] = mouse;
+  mousePosition = index;
+};
 
 const updateMouseTracker = (currentPosition) => {
-    if (!mouseTracker.includes(currentPosition)) { 
-        mouseTracker.push(parseInt(currentPosition))
-    }
-}
+  if (!mouseTracker.includes(currentPosition)) {
+    mouseTracker.push(parseInt(currentPosition));
+  }
+};
 
 const checkIfWinner = () => {
-    if (!winner) {
-        if (mouseTracker.every(num => winningCombo.includes(num) && mouseTracker.length === 9)) {
-            winner = true
-            loser = false
-            mouseScore += 1
-            mouseTally.innerText = `Mouse Wins: ${mouseScore}`
-            gameOver = true
-
-        } else if (catPosition === mousePosition) {
-            winner = false
-            loser = true
-            catScore += 1
-            catTally.innerText = `Cat Wins: ${catScore}`
-            gameOver = true
-    } 
-}
-}
+  if (!winner) {
+    if (
+      mouseTracker.every(
+        (num) => winningCombo.includes(num) && mouseTracker.length === 9
+      )
+    ) {
+      winner = true;
+      loser = false;
+      mouseScore += 1;
+      mouseTally.innerText = `Mouse Wins: ${mouseScore}`;
+      gameOver = true;
+    } else if (catPosition === mousePosition) {
+      winner = false;
+      loser = true;
+      catScore += 1;
+      catTally.innerText = `Cat Wins: ${catScore}`;
+      gameOver = true;
+    }
+  }
+};
 
 squareEls.forEach((square) => {
   square.addEventListener("click", handleClick);
 });
 
 const reset = () => {
-    resetBtn.addEventListener('click', () => {
+  resetBtn.addEventListener("click", () => {
+    gameOver = false;
+    loser = false;
+    winner = false;
+    mouseTracker = [];
+    init();
+  });
+};
 
-    gameOver = false
-    loser = false
-    winner = false
-    mouseTracker = []
-    init()
-})
-}
-
-init ()
-reset()
+init();
+reset();
